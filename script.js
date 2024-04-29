8a069951c5d80cc5a11bcdceafb1edf698b4485e
franciscocostadev <162455503+franciscocostadev@users.noreply.github.com>
2024-04-29 16:43:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = ""
+}
+
+function encriptar (stringEncriptada) {
+
+
+    let matrizCodigo = [["e", "enter" ], ["i", "imes" ], ["a", "ai" ],["o", "ober" ], ["u", "ufat" ] ]; 
+
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceALL(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+
+}
